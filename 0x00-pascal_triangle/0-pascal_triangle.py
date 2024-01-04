abbbc6c2ff1c5babee2f956b0358b70e4708e49b
saaid323 <saaid2538@.com>
2024-01-04 10:39:09 -0800

    added 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    original = [[1], [1, 1]]
+    left_index = 1
+    right_index = 1
+    temp = original[-1]
+    emp = []
+    n = n - 2
+    for i in range(n):
+        for i in range(len(temp)):
+            if len(temp) - 1 > i:
+                emp.append(temp[i] + temp[i + 1])
+        emp.append(left_index)
+        emp.insert(0, right_index)
+        original.append(emp)
+        emp = []
+        temp = original[-1]
+    return original
