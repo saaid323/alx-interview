b5452138672e0aa34dfdc37cd090f9ed680a4548
saaid323 <saaid2538@.com>
2024-01-09 09:11:14 -0800

    added 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" method that determines if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    '''determines if all the boxes can be opened.'''
+    if len(boxes) == 1:
+        return True
+    dic = {0: boxes[0]}
+    keys = [boxes[0]]
+    for i in keys:
+        for j in i:
+            if j not in dic:
+                dic[j] = boxes[j]
+                keys.append(boxes[j])
+    if len(keys) == len(boxes):
+        return True
+    else:
+        return False
