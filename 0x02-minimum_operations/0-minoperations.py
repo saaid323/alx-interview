1825d83db018a0671053299d2b6569f316a18b92
saaid323 <saaid2538@gmail.com>
2024-01-16 20:28:23 +0000

    added 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''calculates the fewest number of operations needed to result in
+exactly n H characters in the file'''
+
+
+def minOperations(n):
+    '''method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file'''
+    store = n
+    total = 0
+    div = 2
+    while div <= n:
+        if n % div == 0:
+            total += div
+            n /= div
+        else:
+            div += 1
+    if store == div and total == store:
+        return 0
+    return total
