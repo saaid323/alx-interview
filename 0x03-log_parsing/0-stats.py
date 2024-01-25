8e53d233a3403c78d299c37822b4e647c9169e0f
saaid323 <saaid2538@gmail.com>
2024-01-25 15:10:16 +0000

    added 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,13 +5,12 @@ import sys
 codes = {}
 size = 0
 store = []
-status_code = ['200', '301', '400', '401', '403', '404', '405', '500']
 
 try:
     for line in sys.stdin:
         code = line.split()[-2]
         last = line.split()[-1]
-        if code in status_code and code not in codes:
+        if code not in codes:
             codes[code] = 1
         else:
             codes[code] += 1
