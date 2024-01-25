654dd6eb3286a20bf386e830e07904b7ec2511fa
saaid323 <saaid2538@gmail.com>
2024-01-25 14:50:30 +0000

    added 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,7 @@
 '''script that reads stdin line by line and computes metrics'''
 import sys
 
-codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+codes = {}
 size = []
 store = []
 
@@ -11,17 +11,17 @@ try:
     for line in sys.stdin:
         code = line.split()[-2]
         last = line.split()[-1]
-        if code in codes.keys():
+        if code not in codes:
+            codes[code] = 1
+        else:
             codes[code] += 1
         store.append(code)
         size.append(int(last))
         if len(store) % 10 == 0:
             print(f'File size: {sum(size)}')
             for k, v in sorted(codes.items()):
-                if v:
                     print(f'{k}: {v}')
 except KeyboardInterrupt:
     print(f'File size: {sum(size)}')
     for k, v in sorted(codes.items()):
-        if v:
             print(f'{k}: {v}')
