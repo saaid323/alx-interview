b3a20f38b7f072b63154ad9612e678eddce4828b
saaid323 <saaid2538@gmail.com>
2024-02-01 07:12:28 +0000

    added 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,24 @@
 def validUTF8(data):
     '''method that determines if a given data set represents a
     valid UTF-8 encoding'''
-    for i in data:
-        if len(bin(i)) > 10:
-            return False
+    remaining = 0
+
+    for num in data:
+        if remaining:
+            if (num >> 6) != 0b10:
+                return False
+            remaining -= 1
+        else:
+            if num >> 7 == 0:
+                remaining = 0
+            elif num >> 5 == 0b110:
+                remaining = 1
+            elif num >> 4 == 0b1110:
+                remaining = 2
+            elif num >> 3 == 0b11110:
+                remaining = 3
+            else:
+                return False
+    if remaining:
+        return False
     return True
