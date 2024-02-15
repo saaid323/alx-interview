678dec5727d49afd50412cfd3e3ee3a00f20c889
saaid323 <saaid2538@gmail.com>
2024-02-15 08:53:33 -0800

    added 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,7 +6,6 @@ const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(url, function (error, response, body) {
   if (error) console.log(error);
   const content = JSON.parse(body);
-  console.log(content.title);
   const characters = content.characters;
   names(characters);
 });
